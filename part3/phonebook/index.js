010264962a142958c6181fcd3a8eb6f8eced0534
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-10-28 09:55:10 +0300

    3.5: Phonebook backend step 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = 
 [
     { 
@@ -51,6 +53,26 @@ app.delete('/api/persons/:id', (request,response)=>{
     persons = persons.filter(p=>p.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons',(request,response)=>{
+    const body = request.body
+
+    if(!body.name || !body.number)
+    {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person ={
+        id: Math.floor(Math.random() * 1000000).toString(),
+        name: body.name,
+        number:body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const port = 3001
