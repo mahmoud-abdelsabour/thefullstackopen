3e407a0c0e7f651e9076986c641a6c3d98ddbf09
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-11-17 22:11:41 +0200

    3.12: Command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3){
+    console.log('give password as arg')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://ma8073676_db_user:${password}@cluster0.e5g7jlb.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url, {family: 4})
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length === 3){
+    Person.find({}).then(result => {
+        result.forEach(person =>{
+            console.log(person)
+        })
+        mongoose.connection.close()
+  })
+}else if(process.argv.length >= 5){
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(result =>{
+        console.log(`added ${process.argv[3]} ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+}else{
+    console.log('You should enter the phone number associated with the name')
+    process.exit(1)
+}
