895d7e14a99af21a4aa961cdc9fed40a95c451de
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-11-24 00:32:50 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- part4/blog-list/backend/utils/list_helper.js
+++ part4/blog-list/backend/utils/list_helper.js
@@ -1,5 +1,9 @@
 const dummy = blogs => {
     return 1
 }
-listHelper = { dummy }
+
+const totalLikes = blogs => blogs.reduce((total, curr) => total + curr.likes, 0)
+
+listHelper = { dummy, totalLikes }
+
 module.exports = listHelper
