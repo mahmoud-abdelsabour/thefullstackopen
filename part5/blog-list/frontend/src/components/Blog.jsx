7b872b7fb2f77999902a4b7cce1a41f36c905225
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-12-16 16:03:43 +0200

    5.7 Blog List Frontend, step 7

--- part5/blog-list/frontend/src/components/Blog.jsx
+++ part5/blog-list/frontend/src/components/Blog.jsx
@@ -1,7 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} <strong>{blog.author}</strong>
-  </div>  
-)
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <strong>{blog.author}</strong>
+      </div>
+      <Togglable buttonLabel='view'>
+        <strong>URL: </strong>{blog.url}<br/>
+        <strong>Likes: </strong>{blog.likes}<button>like</button><br/>
+        <strong>Author: </strong>{blog.author}<br/>
+      </Togglable>
+    </div>
+    
+  )
+}
 
 export default Blog
