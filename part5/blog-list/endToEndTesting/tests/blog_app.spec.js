45130a57b6a45090dbca6c71488019138511e37a
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-12-18 19:56:29 +0200

    5.18: Blog List End To End Testing, step 2

--- part5/blog-list/endToEndTesting/tests/blog_app.spec.js
+++ part5/blog-list/endToEndTesting/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+
+        await request.post('http://localhost:3001/api/testing/reset')
+        await request.post('http://localhost:3001/api/users', {
+            data: {
+                name: 'root',
+                username: 'rootuser',
+                password: 'root'
+            }
+        })
         await page.goto('http://localhost:5173')
     })
 
@@ -9,4 +18,23 @@ describe('Blog app', () => {
         const locator = page.getByText('Log In')
         await expect(locator).toBeVisible()
     })
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('rootuser')
+            await page.getByLabel('password').fill('root')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('root logged in')).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await page.getByLabel('username').fill('rootuser')
+            await page.getByLabel('password').fill('wrong')
+            await page.getByRole('button', { name: 'login' }).click()
+
+            await expect(page.getByText('wrong credentials')).toBeVisible()
+        })
+    })
+
 })
