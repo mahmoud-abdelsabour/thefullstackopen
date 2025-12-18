0c4cbd4ff574ff78eec9fd86adbd2494dc02b010
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-12-18 20:18:46 +0200

    5.19: Blog List End To End Testing, step 3

--- part5/blog-list/endToEndTesting/tests/blog_app.spec.js
+++ part5/blog-list/endToEndTesting/tests/blog_app.spec.js
@@ -37,4 +37,22 @@ describe('Blog app', () => {
         })
     })
 
+    describe('When logged in', () => {
+        beforeEach(async ({ page }) => {
+            await page.getByLabel('username').fill('rootuser')
+            await page.getByLabel('password').fill('root')
+            await page.getByRole('button', { name: 'login' }).click()
+        })
+
+        test('a new blog can be created', async ({ page }) => {
+            await page.getByRole('button', { name: 'Create New Blog' }).click()
+            await page.getByLabel('Title').fill('test blog')
+            await page.getByLabel('Author').fill('John Doe')
+            await page.getByLabel('URL').fill('example.com')
+            await page.getByRole('button', { name: 'Add' }).click()
+
+            await expect(page.locator('div.title.and.author.div').getByText('test blog')).toBeVisible()
+        })
+    })
+
 })
