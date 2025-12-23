3832a07fba40abd320b4035a9588a49d31e6f52b
mahmoud-abdelsabour <mahmoud.ahmed63@msa.edu.eg>
2025-12-23 17:41:41 +0200

    6.9 Anecdotes, step 7

---
+++ part6/redux-anecdotes-main/src/reducers/filterReducer.js
@@ -0,0 +1,19 @@
+const filterReducer = (state = '', action) => {
+    switch (action.type){
+        case 'SET_FILTER':
+            return action.payload.filter
+        default:
+            return state
+    }
+}
+
+export const setFilter = filter => (
+    {
+        type: 'SET_FILTER',
+        payload: {
+            filter: filter
+        }
+    }
+)
+
+export default filterReducer
